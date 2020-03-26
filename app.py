b4dff7b98504ee8fc94fd2bdffd732fd673c90dc
Charles Kantz <kantz.charles@gmail.com>
2020-03-25 17:57:22 -0700

    mostly operational, DELETE function still WIP

--- app.py
+++ app.py
@@ -1,25 +1,94 @@
 """Blogly application."""
 
-from flask import Flask, render_template
-from models import db, connect_db, User
+from flask import Flask, render_template, redirect, request, flash
+from models import db, connect_db, User, add_user, update_user, delete_user
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SECRET!"
 
 connect_db(app)
 db.create_all()
-
-from flask_debugtoolbar import DebugToolbarExtension
-app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    "index page, redirects to '/users'"
+    return redirect('/users')
+
 
 @app.route('/users')
 def user_listing():
-    return render_template('user-listing.html', title='hello')
+    "Show list of all users"
+
+    users = User.query.all()
+
+    return render_template('user-listing.html', title='Users', users=users)
+
+
+@app.route('/users/new')
+def create_user():
+    "Create a new user"
+    return render_template('new-user-form.html', title="Create a User")
+
+
+@app.route('/users/new', methods=["POST"])
+def store_new_user():
+    "Accepts POST request of new user"
+
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+
+    add_user(first_name, last_name, image_url)
+
+    flash(f"{first_name} {last_name} was added as a user.")
+    return redirect('/users')
+
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+    "generate user detail page"
+
+    user = User.query.filter(User.id == int(user_id)).one()
+
+    return render_template('user-detail-page.html',
+                           title=f"{user.first_name} {user.last_name}",
+                           user=user)
+
+
+@app.route('/users/<user_id>/edit')
+def edit_user(user_id):
+
+    user = User.query.filter(User.id == int(user_id)).one()
+
+    return render_template('user-edit.html',
+                           title=f"{user.first_name} {user.last_name}",
+                           user=user)
+
+
+@app.route('/users/<user_id>/edit', methods=['POST'])
+def store_edits(user_id):
+
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+
+    update_user(user_id, first_name, last_name, image_url)
+
+    flash(f"{first_name} {last_name} information updated.")
+    return redirect('/users')
+
+
+@app.route('/users/<user_id>/delete', methods=['DELETE'])
+def delete_user(user_id):
+    "Delete the user"
+
+    delete_user(user_id)
+
+    flash("User deleted.")
+    return redirect('/')
--- models.py
+++ models.py
@@ -12,13 +12,50 @@ def connect_db(app):
     db.init_app(app)
 
 
+def add_user(first_name, last_name, image_url):
+    "collects new user, adds it to session, and commits to DB"
+
+    user = User(first_name=first_name,
+                last_name=last_name,
+                image_url=image_url)
+
+    if not user.image_url:
+        user.image_url = None
+
+    db.session.add(user)
+    db.session.commit()
+
+
+def update_user(id, first_name, last_name, image_url):
+    "collects updated user info and commits to DB"
+
+    user = User.query.filter(User.id == int(id)).one()
+
+    user.first_name = first_name
+    user.last_name = last_name
+
+    if not image_url:
+        image_url = 'https://i.imgur.com/XVicMmG.jpg'
+
+    user.image_url = image_url
+
+    db.session.commit()
+
+
+def delete_user(id):
+    "delete a user and commit to DB"
+
+    User.query.filter(User.id == id).delete()
+
+    db.session.commit()
+
+
 class User(db.Model):
     '''User'''
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String, nullable=False)    
+    first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
-    image_url = db.Column(db.String, default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
-
+    image_url = db.Column(db.String, default='https://i.imgur.com/XVicMmG.jpg')
